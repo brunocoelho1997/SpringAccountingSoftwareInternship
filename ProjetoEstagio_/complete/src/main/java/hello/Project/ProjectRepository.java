ec60d8ae5e9213f8d7eea977011c3382d4aa7422
BrunoCoelho <brunocoelho1997@gmail.com>
2018-04-25 09:08:39 +0100

    Commit antes de qualquer alteração feita após a reunião

--- ProjetoEstagio_/complete/src/main/java/hello/Project/ProjectRepository.java
+++ ProjetoEstagio_/complete/src/main/java/hello/Project/ProjectRepository.java
@@ -3,4 +3,6 @@ package hello.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Project findByProjectClient(ProjectClient projectClient);
 }
